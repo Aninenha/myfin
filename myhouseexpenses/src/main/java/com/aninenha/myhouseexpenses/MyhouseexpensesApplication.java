2bcf67d6ee65905c9873c919e81f0cecd4015dd6
Aninenha <anaflaviafiorini@gmail.com>
2026-03-16 10:17:12 -0300

    spring initialize + postgreeSQL

---
+++ myhouseexpenses/src/main/java/com/aninenha/myhouseexpenses/MyhouseexpensesApplication.java
@@ -0,0 +1,13 @@
+package com.aninenha.myhouseexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhouseexpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhouseexpensesApplication.class, args);
+	}
+
+}
---
+++ myhouseexpenses/src/test/java/com/aninenha/myhouseexpenses/MyhouseexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aninenha.myhouseexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhouseexpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
